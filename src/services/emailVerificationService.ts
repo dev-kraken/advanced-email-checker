f9ab6ac76184f22e95db35f6575c4c4a94982cd1
Dev Kraken <soman@devkraken.com>
2024-06-25 03:46:59 +0400

    db conneciton fixed and stmp service on

--- src/services/emailVerificationService.ts
+++ src/services/emailVerificationService.ts
@@ -1,6 +1,7 @@
 import dns from 'dns';
 import {promisify} from 'util';
 import validator from 'validator';
+import nodemailer from 'nodemailer';
 import logger from '../utils/logger';
 import axios from 'axios';
 import prisma from '../config/prismaClient';
@@ -19,6 +20,8 @@ interface VerificationResult {
 
 const resolveMx = promisify(dns.resolveMx);
 
+const disposableDomainCache = new Set<string>();
+
 async function checkMxRecords(domain: string): Promise<boolean> {
     try {
         const records = await resolveMx(domain);
@@ -29,66 +32,70 @@ async function checkMxRecords(domain: string): Promise<boolean> {
     }
 }
 
-// async function checkSmtp(email: string): Promise<boolean> {
-//     // const [user, domain] = email.split('@');
-//     // try {
-//     //     const records = await resolveMx(domain);
-//     //     if (records.length === 0) return false;
-//     //
-//     //     const mxHost = records[0].exchange;
-//     //
-//     //     const transporter = nodemailer.createTransport({
-//     //         host: mxHost,
-//     //         port: 25,
-//     //         secure: false, // use TLS
-//     //         tls: {
-//     //             rejectUnauthorized: false
-//     //         },
-//     //         connectionTimeout: 5000 // 5 seconds
-//     //     });
-//     //
-//     //     return new Promise((resolve) => {
-//     //         transporter.verify((error) => {
-//     //             if (error) {
-//     //                 logger.error(`SMTP validation failed for email ${email}: ${error.message}`);
-//     //                 resolve(false);
-//     //             } else {
-//     //                 resolve(true);
-//     //             }
-//     //         });
-//     //     });
-//     // } catch (err) {
-//     //     logger.error(`SMTP validation failed for email ${email}: ${(err as Error).message}`);
-//     //     return false;
-//     // }
-//     return false;
-// }
+async function checkSmtp(email: string): Promise<boolean> {
+    const [user, domain] = email.split('@');
+    try {
+        const records = await resolveMx(domain);
+        if (records.length === 0) return false;
+
+        const mxHost = records[0].exchange;
+        console.log(mxHost)
+        const transporter = nodemailer.createTransport({
+            host: mxHost,
+            port: 25,
+            secure: false, // use TLS
+            tls: {
+                rejectUnauthorized: false
+            },
+            connectionTimeout: 5000 // 5 seconds
+        });
+
+        return new Promise((resolve) => {
+            transporter.verify((error) => {
+                if (error) {
+                    logger.error(`SMTP validation failed for email ${email}: ${error.message}`);
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    } catch (err) {
+        logger.error(`SMTP validation failed for email ${email}: ${(err as Error).message}`);
+        return false;
+    }
+}
 
 async function isDisposable(email: string): Promise<boolean> {
     const domain = email.split('@')[1];
 
+    if (disposableDomainCache.has(domain)) {
+        return true;
+    }
 
     const disposableDomain = await prisma.tempEmailDomains.findFirst({
         where: {emailDomain: domain}
     });
-    console.log(disposableDomain)
-    if (!disposableDomain) {
 
-        try {
-            const response = await axios.get(`https://disposable.debounce.io/?email=${email}`);
-            const isDisposable = response.data.disposable === 'true';
-
-            if (isDisposable) {
-                await prisma.tempEmailDomains.create({data: {emailDomain: domain}});
-            }
-
-            return isDisposable;
-        } catch (error) {
-            logger.error('Failed to validate email:', error);
-            return false;
-        }
+    if (disposableDomain) {
+        disposableDomainCache.add(domain);
+        return true;
     }
-    return true
+
+    try {
+        const response = await axios.get(`https://disposable.debounce.io/?email=${email}`);
+        const isDisposable = response.data.disposable === 'true';
+
+        if (isDisposable) {
+            await prisma.tempEmailDomains.create({data: {emailDomain: domain}});
+            disposableDomainCache.add(domain);
+        }
+
+        return isDisposable;
+    } catch (error) {
+        logger.error('Failed to validate email:', error);
+        return false;
+    }
 }
 
 async function verifyEmail(email: string): Promise<VerificationResult> {
@@ -106,21 +113,21 @@ async function verifyEmail(email: string): Promise<VerificationResult> {
         };
     }
 
-    const [isDisposableEmail, hasMxRecords] = await Promise.all([
+    const [isDisposableEmail, hasMxRecords, smtpValid] = await Promise.all([
         isDisposable(email),
         checkMxRecords(email.split('@')[1]),
-        // checkSmtp(email)
+        checkSmtp(email)
     ]);
 
     const result: VerificationResult = {
         is_disposable: isDisposableEmail,
         has_mx_records: hasMxRecords,
-        smtp_valid: false,
+        smtp_valid: smtpValid,
         errors: {
             syntax: null,
             disposable_email: isDisposableEmail ? 'Email is from a disposable email provider' : null,
             mx_records: hasMxRecords ? null : 'Domain does not have valid MX records',
-            smtp: 'SMTP validation failed',
+            smtp: smtpValid ? null : 'SMTP validation failed',
         },
     };
 
